ed40c87e2a9dac30215d6f713257f504d8079226
J0aoCunha <jvjunger20123@gmail.com>
2024-01-30 19:16:36 -0300

    chore: configurando dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { app} from "./app";
+import { Env } from "./env";
 
 app.listen({
   host: '0.0.0.0',
-  port: 3333
+  port: Env.data.PORT
 }).then(()=>{
   console.log('⚠️ Server is running')
 })
