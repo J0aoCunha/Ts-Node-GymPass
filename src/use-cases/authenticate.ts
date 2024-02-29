d097c2d44e9d2c661d020c53f1ea491d3d9fb4d1
J0aoCunha <jvjunger20123@gmail.com>
2024-02-29 01:32:17 -0300

    feat: criando validacao dos dados do usuario

--- src/use-cases/authenticate.ts
+++ src/use-cases/authenticate.ts
@@ -3,34 +3,33 @@ import { User } from '@prisma/client';
 import { InvalidCredentialsError } from './errors/invalid-credentials-error';
 import { compare } from 'bcryptjs';
 
-
 interface AuthenticateUseCaseRequest {
   email: string
   password: string
 }
 interface AuthenticateUseCaseResponse {
-	user: User
+  user: User
 }
 
-export class AuthenticateUseCase{
-	constructor(
-    private usersRespository: UsersRepository
-	){}
+export class AuthenticateUseCase {
+	constructor(private UsersRespository: UsersRepository) {}
 
-	async execute({ email, password }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse>{
-		const user = await this.usersRespository.findByEmail(email);
+	async execute({
+		email,
+		password,
+	}: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+		const user = await this.UsersRespository.findByEmail(email);
 
-		if(!user){
+		if (!user) {
 			throw new InvalidCredentialsError();
 		}
 
 		const doesPasswordMatches = await compare(password, user.password_hash);
 
-		if(!doesPasswordMatches){
+		if (!doesPasswordMatches) {
 			throw new InvalidCredentialsError();
 		}
 
-		return {user};
+		return { user };
 	}
-
 }
--- src/use-cases/factories/make-authenticate-use-case.ts
+++ src/use-cases/factories/make-authenticate-use-case.ts
@@ -1,11 +1,9 @@
-import { prismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
 import { AuthenticateUseCase } from '../authenticate';
 
-
-export function makeAuthenticateUseCase(){
-	const UserRepository = new prismaUsersRepository();
+export function makeAuthenticateUseCase() {
+	const UserRepository = new PrismaUsersRepository();
 	const authenticateUseCase = new AuthenticateUseCase(UserRepository);
-
 
 	return authenticateUseCase;
 }
